a6395bdbd8df38e546fdc50ab60296cdc10dc927
scott1028 <mic1028002@gmail.com>
2013-09-06 17:08:34 +0800

    modified:   a_star.html 	modified:   scene_shader.js 	aStar.js 	objects.js 	stage.html 	stage.js

--- scene_shader.js
+++ scene_shader.js
@@ -41,13 +41,13 @@
 		var p=$('<span class="p"></span>').html('p').css('color','blue');
 		var t=$('<span class="t"></span>').html('t').css('color','red');
 
-		p.appendTo($('td[pos=1-4]'));
+		p.appendTo($('td[pos=1-2]'));
 		t.appendTo($('td[pos=9-9]'));
 
 		return {
 			p:{
-				x:parseInt($('td[pos=1-4]').attr('pos').split('-')[0]),
-				y:parseInt($('td[pos=1-4]').attr('pos').split('-')[1]),
+				x:parseInt($('td[pos=1-2]').attr('pos').split('-')[0]),
+				y:parseInt($('td[pos=1-2]').attr('pos').split('-')[1]),
 			},
 			t:{
 				x:parseInt($('td[pos=9-9]').attr('pos').split('-')[0]),
